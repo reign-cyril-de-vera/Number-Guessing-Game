3e196ca862150f2fb4a3db9babc68d97848d434e
Reign Cyril De Vera <rhdevera@up.edu.ph>
2023-10-02 03:05:06 -0400

    feat: add comments for code readability

--- number_guess.sh
+++ number_guess.sh
@@ -5,6 +5,8 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --tuples-only -c"
 
 # FUNCTIONS
 
+
+# Function of getting the user's history in the database
 CHECK_USER_HISTORY(){
   local username="$1"
   local query="SELECT COUNT(*), MIN(num_guess)
@@ -14,6 +16,8 @@ CHECK_USER_HISTORY(){
   $PSQL "$query"
 }
 
+
+# Function of adding game user's game details to the database
 INSERT_GAME_DETAILS(){
   local username="$1"
   local num_guess="$2"
@@ -26,25 +30,34 @@ INSERT_GAME_DETAILS(){
 
 # MAIN
 
-RANDOM_NUM=$((RANDOM % 1001))
 
+# Getting the username
 echo "Enter your username:"
 read USERNAME
 
+
+# Getting user history if present
 USER_HISTORY="$(CHECK_USER_HISTORY "$USERNAME")"
 read GAMES_PLAYED BAR NUM_GUESS_BEST <<< "$USER_HISTORY"
 
+
+# Message whether or not user history is present
 if [[ -z $USER_HISTORY ]]; then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $NUM_GUESS_BEST guesses."
 fi
 
+
+# Initializing the number guessing game
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
+RANDOM_NUM=$((RANDOM % 1001))
 GUESSED=false
 NUM_GUESS=0
 
+
+# Loop for number guessing game
 while [ "$GUESSED" = false ]; do
   if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
@@ -64,4 +77,6 @@ while [ "$GUESSED" = false ]; do
   fi
 done
 
+
+# Add the game details to database
 ADD_TO_DATABASE="$(INSERT_GAME_DETAILS "$USERNAME" $NUM_GUESS)"
